60e94870bb1a47535d33431aa8575ac73d6cbb74
kadamski <kamil.adamski@thefarm51.com>
2023-08-23 10:20:43 +0200

    Ticketing initial commit

---
+++ auth/src/index.ts
@@ -0,0 +1,13 @@
+import exppress from 'express';
+import { json } from 'body-parser';
+
+const app = exppress();
+app.use(json());
+
+app.get('/api/users/currentuser', (req, res) => {
+	res.send('Hi there!!');
+});
+
+app.listen(3000, () => {
+	console.log('Listen on port 3000');
+});
