52c30b982a93a1d6541636cdb09e06425fbbed6e
kadamski <kamil.adamski@thefarm51.com>
2023-08-23 12:49:29 +0200

    adding express validator

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,17 @@
 import exppress from 'express';
 import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current-user';
+import { singinRouter } from './routes/singin';
+import { singoutRouter } from './routes/singout';
+import { singupRouter } from './routes/singup';
 
 const app = exppress();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-	res.send('Hi there!!');
-});
+app.use(currentUserRouter);
+app.use(singinRouter);
+app.use(singoutRouter);
+app.use(singupRouter);
 
 app.listen(3000, () => {
 	console.log('Listen on port 3000');
