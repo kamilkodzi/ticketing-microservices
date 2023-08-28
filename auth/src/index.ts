7f1e9022e86c4fa8b06ffe996cbdc8de05fd9c30
kadamski <kamil.adamski@thefarm51.com>
2023-08-28 09:49:31 +0200

    hashing password before save

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import exppress from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from './routes/current-user';
 import { singinRouter } from './routes/singin';
 import { singoutRouter } from './routes/singout';
@@ -20,6 +22,17 @@ app.all('*', async (req, res) => {
 });
 app.use(errorHandler);
 
-app.listen(3000, () => {
-	console.log('Listen on port 3000');
-});
+const start = async () => {
+	try {
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+		console.log("Connected to mongo DB")
+	} catch (error) {
+		console.log(error);
+	}
+
+	app.listen(3000, () => {
+		console.log('Listen on port 3000');
+	});
+};
+
+start();
---
+++ auth/src/models/user.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+import { Password } from '../services/password';
+
+// An interface that describe the properties
+// that are require to create new User
+interface UserArrts {
+	email: string;
+	password: string;
+}
+
+// An nterface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+	build(attrs: UserArrts): UserDoc;
+}
+
+// An Interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+	email: string;
+	password: string;
+}
+
+const userSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		require: true,
+	},
+	password: {
+		type: String,
+		require: true,
+	},
+});
+
+userSchema.pre('save', async function (done) {
+	if (this.isModified('password')) {
+		const hashed = await Password.toHash(this.get('password') as string);
+		this.set('password', hashed);
+	}
+	done();
+});
+
+userSchema.statics.build = (atrrs: UserArrts) => {
+	return new User(atrrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
