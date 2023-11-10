ecb7dbc04b65a1777aadd3ac86ff10e2f4880e18
kamilkodzi <bizkamiladamski@gmail.com>
2023-11-10 08:27:30 +0100

    splitting app in to two files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-## TBD Soon
+## TBD more - soon
+ 
+ Start:
+ 1. Inform how to add new host to host file.
+ 2. Docker + kubernetes up and runnig
+ 3. Nginx server in kubernetes cluster up and runnig
+ 4. Add env variables in kubernetes 
+ 5. Install skaffold
  Running dev with hot reload - `skaffold dev`
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,34 +1,5 @@
-import exppress from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { singinRouter } from './routes/singin';
-import { singoutRouter } from './routes/singout';
-import { singupRouter } from './routes/singup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = exppress();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-	cookieSession({
-		signed: false,
-		secure: true,
-	})
-);
-
-app.use(currentUserRouter);
-app.use(singinRouter);
-app.use(singoutRouter);
-app.use(singupRouter);
-app.all('*', async (req, res) => {
-	throw new NotFoundError();
-});
-app.use(errorHandler);
+import { app } from './app'
 
 const start = async () => {
 	if (!process.env.JWT_KEY) {
