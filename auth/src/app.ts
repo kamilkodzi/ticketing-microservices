6ad13e46c5c92849403243cf019c05aeceec863a
Kamil <33725897+kamilkodzi@users.noreply.github.com>
2023-11-16 11:38:33 +0100

    Test initialization (#1)
    * splitting app in to two files
    
    * auth testing

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import exppress from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { singinRouter } from './routes/signin';
+import { singoutRouter } from './routes/signout';
+import { singupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = exppress();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test',
+    })
+);
+
+app.use(currentUserRouter);
+app.use(singinRouter);
+app.use(singoutRouter);
+app.use(singupRouter);
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+app.use(errorHandler);
+
+export { app }
---
+++ auth/src/test/setup.ts
@@ -0,0 +1,51 @@
+import { MongoMemoryServer } from "mongodb-memory-server"
+import mongoose from "mongoose"
+import request from 'supertest'
+import { app } from '../app'
+
+declare global {
+    var signin: () => Promise<string[]>;
+}
+
+let mongo: MongoMemoryServer | null
+
+beforeAll(async () => {
+    process.env.JWT_KEY = 'asaasas'
+    mongo = await MongoMemoryServer.create()
+    const mongoUri = mongo.getUri()
+
+    await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+    const collectios = await mongoose.connection.db.collections()
+
+    for (let collection of collectios) {
+        await collection.deleteMany({})
+    }
+})
+
+afterAll(async () => {
+    if (mongo) {
+        await mongo.stop();
+    }
+    await mongoose.connection.close();
+});
+
+
+
+signin = async () => {
+    const email = 'test@test.com'
+    const password = 'password'
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,
+            password
+        })
+        .expect(201)
+    const cookie = response.get('Set-Cookie');
+
+    return cookie
+}
