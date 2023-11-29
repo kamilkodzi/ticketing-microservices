7a06e5b21bc3982cf79f55323e50b7b0f1433b1b
kamilkodzi <bizkamiladamski@gmail.com>
2023-11-29 18:04:13 +0100

    front part with client

---
+++ client/api/build-client.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const buildClient = ({ req }) => {
+  if (typeof window === "undefined") {
+    // We are on server
+
+    return axios.create({
+      baseURL:
+        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      headers: req.headers,
+    });
+  } else {
+    // We are on browser
+    return axios.create({
+      baseURL: "/",
+    });
+  }
+};
+
+export default buildClient;
---
+++ client/pages/index.js
@@ -0,0 +1,14 @@
+import buildClient from "../api/build-client";
+
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? <h1>You are signin</h1> : <h1>You are NOT signin !</h1>;
+};
+
+LandingPage.getInitialProps = async (contex) => {
+  const client = buildClient(contex);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
+};
+
+export default LandingPage;
