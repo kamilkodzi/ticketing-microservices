6839c60451806b35eba4ace7f02775392acb4854
kamilkodzi <bizkamiladamski@gmail.com>
2023-12-02 12:16:50 +0100

    Tickets api facade

---
+++ tickets/src/app.ts
@@ -0,0 +1,22 @@
+import exppress from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+import { errorHandler, NotFoundError } from '@katicketing/common'
+
+const app = exppress()
+app.set('trust proxy', true)
+app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+)
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError()
+})
+app.use(errorHandler)
+
+export { app }
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,7 +6,7 @@ const start = async () => {
     throw new Error('JWT_KEY must be defined')
   }
   if (!process.env.MONGO_URI) {
-    throw new Error('MONGO_URI must be defined')
+    throw new Error('JWT_KEY must be defined')
   }
   try {
     await mongoose.connect(process.env.MONGO_URI)
