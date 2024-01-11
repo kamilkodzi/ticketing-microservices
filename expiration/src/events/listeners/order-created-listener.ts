790b456c32fc53a49457e385fbe10d7a7cd806ee
kamilkodzi <bizkamiladamski@gmail.com>
2024-01-11 13:35:24 +0100

    adding queue with Bull js

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,17 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@katicketing/common'
+import { queueGroupName } from './queue-group-name'
+import { Message } from 'node-nats-streaming'
+import { expirationQueue } from '../../queues/expiration-queue'
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated
+  queueGrouName = queueGroupName
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime()
+    console.log('Waiting this many miliseconds to process the job: ', delay)
+
+    await expirationQueue.add({ orderId: data.id }, { delay })
+    msg.ack()
+  }
+}
